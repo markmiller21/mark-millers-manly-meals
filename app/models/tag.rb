8815f39f731f83374203530b9479794ead139a49
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-01-10 17:11:28 -0500

    build basic post and tags to prove DB is setup correctly

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ApplicationRecord
 	has_many :taggings
 	has_many :posts, through: :taggings
+
+	# Method to get the number of tags to this reference
+	def self.counts
+	  self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+	end
 end
